6783b3e8680c10429d23741e883f1c2f771e1713
RounaK <itzrounak800@gmail.com>
2026-06-23 18:03:43 +0530

    Improved responsiveness and added light mode
    Now the app will adapt to screen sizes of different devices.
    And added a light mode theme and button to switch theme modes.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const themeBtn = document.getElementById('theme-btn')
+const darkSvg = document.getElementById('dark-svg')
+const lightSvg = document.getElementById('light-svg')
 const incrementBtn = document.getElementById('js-increment')
 const decrementBtn = document.getElementById('js-decrement')
 const resetBtn = document.getElementById('js-reset')
@@ -6,14 +9,29 @@ const counter = document.getElementById('counter')
 let count = 0
 let darkMode = true
 
+themeBtn.addEventListener('click', () => {
+    darkMode = !darkMode
+    document.body.classList.toggle('light-mode')
+
+    if(!darkMode) {
+        darkSvg.style.display = 'block'
+        lightSvg.style.display = 'none'
+    } else {
+        darkSvg.style.display = 'none'
+        lightSvg.style.display = 'block'
+    }
+})
+
 incrementBtn.addEventListener('click', () => {
     count++
     counterDisplay()
 })
 
 decrementBtn.addEventListener('click', () => {
-    count--
-    counterDisplay()
+    if(count > 0) {
+        count--
+        counterDisplay()
+    }
 })
 
 resetBtn.addEventListener('click', () => {
